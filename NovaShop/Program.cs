fb8f744cc14e319ec5b54d894f4a6e222b85b708
Lisandro <vlisandrofor@yahoo.com.ar>
2026-05-05 19:57:59 -0300

    Agregar archivos de proyecto.

---
+++ NovaShop/Program.cs
@@ -0,0 +1,32 @@
+using NovaShop.Extensions;
+
+
+public partial class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var builder = WebApplication.CreateBuilder(args);
+
+
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Swagger UI
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        // Endpoints
+        app.MapItemEndpoints();
+
+        app.Run();
+    }
+}
