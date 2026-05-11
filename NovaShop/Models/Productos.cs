1c6698d4dfa593501481bb35d2328ab13193e346
Lisandrofor <58186709+Lisandrofor@users.noreply.github.com>
2026-05-11 12:18:28 -0300

    Actualizar Productos.cs

--- NovaShop/Models/Productos.cs
+++ NovaShop/Models/Productos.cs
@@ -1,6 +1,15 @@
 ﻿namespace NovaShop.Models
 {
-    public class Productos
+    public record Productos
     {
+      public Guid Id {get; set;}
+      public String Descripcion {get; set;}
+      public Data time Alta {get; set;}
+      public int Stock {get; set;}
+      public Guid Id {get; set;}
+
+      
     }
+
+
 }
