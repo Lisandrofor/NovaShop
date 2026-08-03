2b2a0f9ed6c2aa12bd8195d8086a03b7b39e5a49
Lisandrofor <58186709+Lisandrofor@users.noreply.github.com>
2026-08-03 10:31:34 -0300

    Actualizar Program.cs

--- NovaShop/Program.cs
+++ NovaShop/Program.cs
@@ -22,7 +22,7 @@ public partial class Program
         builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
         builder.Services.AddScoped<IProductosRepository, ProductoRepository>();
         builder.Services.AddScoped<ICarritoRepository,CarritoRepository >();
-        builder.Servicee.AddScoped<IOrden
+        builder.Servicee.AddScoped<ICompraRepository, CompraRepository>();
 
         builder.AddAppLogging();
 
